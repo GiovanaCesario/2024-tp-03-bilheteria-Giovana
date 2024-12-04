3c959363e725b48b0ec0fc78ca01d379464de4c1
GiovanaCesario <a2023951504@teiacoltec.org>
2024-12-04 16:04:31 -0300

    Pronto

--- src/ingresso/Ingresso.java
+++ src/ingresso/Ingresso.java
@@ -24,7 +24,7 @@ public class Ingresso implements IReceita {
 
     @Override
     public void extratoReceita() {
-        System.out.println(toString());
+        System.out.println(this);
     }
 
     @Override
